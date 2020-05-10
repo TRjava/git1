7c2e01802833186982a89e527c5a9687ffab3a0f
shinelon <shinelon@DESKTOP-BV4G06A>
2020-05-10 13:10:05 +0800

    one1

---
+++ test1/src/test1/tst.java
@@ -0,0 +1,16 @@
+package test1;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class tst extends ObjectInputStream{
+protected tst() throws IOException, SecurityException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+private void syso() {
+	// TODO Auto-generated method stub
+
+}
+}
